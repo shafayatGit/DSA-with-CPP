9638624a96691b8af79ecd7d9fb5812f16506e7f
shafayatGit <shafayathossain.ielts@gmail.com>
2025-11-21 20:51:19 +0600

    insertion sort

---
+++ Algorithm/InsertionSort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {6,8,12,14,18,10};
+    int j,x;
+    for(int i =1; i<6; i++){
+         j = i-1;
+         x = arr[i];
+        while(j>-1 && arr[j]>x){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = x;
+    }
+
+    for(int i=0; i<6; i++){
+        cout<<arr[i]<<" ";
+    }
+}
