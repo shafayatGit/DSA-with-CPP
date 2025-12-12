75b56a5c6dd1a2ba7cc2657a18c968ea2171b2bf
shafayatGit <shafayathossain.ielts@gmail.com>
2025-12-12 19:22:14 +0600

    push

---
+++ Recursion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void print1toN ( int n ) {
+if( n == 0) return ; // Base case
+print1toN (n -1) ; // Recursive call
+cout << n << " ";
+}
+
+int main(){
+    print1toN(10);
+}
