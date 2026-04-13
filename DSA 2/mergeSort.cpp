8d91e4e99edfd6e095b435508e9fbd5ce36b2d65
shafayatGit <shafayathossain.ielts@gmail.com>
2026-04-13 10:07:45 +0600

    merge sort

---
+++ DSA 2/mergeSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+// Merge Sort
+
+void merge(vector<int> &v, int start, int mid, int end)
+{
+    // left array: start -> mid
+    // right array: mid+1 ->end
+    int i = start;
+    int j = mid + 1;
+}
+
+void mergeSort(vector<int> &v, int start, int end)
+{
+    // base case
+    if (end - start == 1)
+    {
+        if (v[start > v[end]])
+        {
+            swap(v[start], v[end]);
+            return;
+        }
+    }
+    if (start == end)
+    {
+        return;
+    }
+    // devide
+    int mid = (start + end) / 2;
+    mergeSort(v, start, end);
+    mergeSort(v, mid + 1, end);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+}
